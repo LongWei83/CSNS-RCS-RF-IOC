9446b904f3a55492865738f5258b38443220d617
Long Wei <longw@ihep.ac.cn>
2016-02-26 11:13:53 +0800

    2016-2-26 origin CSNS RCS RF Epics IOC

---
+++ rcsRfIoc/rcsRfIocApp/src/O.linux-x86/rcsRfIoc.dbd.d
@@ -0,0 +1,50 @@
+# DO NOT EDIT: This file created by mkmf.pl,v 1.5 2002/03/25 21:33:24 jba Exp $ 
+
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/base.dbd
+../O.Common/rcsRfIoc.dbd : ../devD212.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuGlobal.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuConvert.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/aaiRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/aaoRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/aiRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/aoRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/aSubRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/biRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/boRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/calcRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/calcoutRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/compressRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/dfanoutRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/eventRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/fanoutRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/longinRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/longoutRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/mbbiRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/mbbiDirectRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/mbboRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/mbboDirectRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/permissiveRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/selRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/seqRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/stateRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/stringinRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/stringoutRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/subRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/subArrayRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/waveformRecord.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/devSoft.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuAlarmSevr.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuAlarmStat.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuCompress.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuFtype.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuIvoa.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuOmsl.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuPini.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuPriority.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuScan.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuSimm.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/menuYesNo.dbd
+../O.Common/rcsRfIoc.dbd : /home/RCS/Downloads/base-3.14.12.1/dbd/dbCommon.dbd
+
+
+../O.Common/rcsRfIocInclude.dbd:../Makefile
